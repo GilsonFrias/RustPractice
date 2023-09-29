c58e71f9fe4536e400466760d934d390a03c2a8f
Gilson <gilson@pop-os.localdomain>
2023-09-28 22:37:30 -0400

    practicing with arrays and variables types

--- src/main.rs
+++ src/main.rs
@@ -4,4 +4,14 @@ fn main() {
     println!("The value of _string is: {_string}");
     x = 6;
     println!("The value of x is: {x}");
+    let y:u8 = b'A';
+    println!("The value of y is: {y}");
+    let z:u16 = 0b1111_0000;
+    println!("The value of y is: {z}");
+    let heartEyedCat:char = '😻';
+    println!("The value of heartEyedCat is: {heartEyedCat}");
+    let tuple: (u8, char, bool) = (32, 'F', false);
+    let arr: [i8; 1] = [-9];
+    let first = arr[0];
+    println!("First element in array arr is: {first}");
 }
